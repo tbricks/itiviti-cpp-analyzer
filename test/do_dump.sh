0890b3b564ed82cb2cb3fd60bb435be895d1abcd
Egor Zudin <zudin.0197@gmail.com>
2022-05-07 14:44:44 +0300

    Make do_test.sh more general

--- test/do_dump.sh
+++ test/do_dump.sh
@@ -4,13 +4,13 @@ LLVM_ROOT="/opt/llvm-10"
 GCC_ROOT="/opt/gcc-8.2.0"
 FILTER="-ast-dump-filter=${2:-main}"
 
-${LLVM_ROOT}/bin/clang++ -MJ tmp.o.json --std=c++1z \
-    --gcc-toolchain=/opt/gcc-8.2.0 \
-    -isystem/opt/gcc-8.2.0/lib/gcc/x86_64-linux-gnu/8.2.0/include \
+clang++ -MJ tmp.o.json --std=c++1z \
     -o tmp.o -c $1
 
 sed -e '1s/^/[\n/' -e '$s/,$/\n]/' *.o.json > compile_commands.json
 
-${LLVM_ROOT}/bin/clang-check -extra-arg=-std=c++1z -ast-dump $FILTER $1
+clang-check -extra-arg=-std=c++1z -ast-dump $FILTER $1
+
+rm compile_commands.json tmp.o.json
 
 #${LLVM_ROOT}/bin/clang++ -Xclang -ast-dump -fsyntax-only --std=c++1z $1
