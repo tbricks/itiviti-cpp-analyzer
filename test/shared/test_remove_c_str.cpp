5488ef20aac592b65f934c4a4f7300d88b0c3fce
Bamboo <bamboo@itiviti.com>
2022-06-07 13:15:54 +0300

    [Stash -> Github] sync commit

--- test/shared/test_remove_c_str.cpp
+++ test/shared/test_remove_c_str.cpp
@@ -2,38 +2,19 @@
 #include <vector>
 #include <string_view>
 
-namespace tbricks::types {
-
-struct StringRef
-{
-    std::string to_string() const { return {}; }
-};
-
-struct VIID
+struct Container
 {
     int set_ref(const char * key, const char * value) { return 0; }
-    int set_ref(const std::vector<StringRef> & key, const char * value) { return 0; }
+    int set_ref(const std::vector<std::string_view> & key, const char * value) { return 0; }
     int set_ref(const std::string_view key, const char * value) { return 0; } // expected-note 2 {{overload defined here}}
 
     int set_view(const char * key, const char * value) { return 0; }
-    int set_view(const StringRef * key, const char * value) { return 0; }
-    int set_view(const StringRef key, const char * value); // expected-note 2 {{overload defined here}}
+    int set_view(const std::string_view * key, const char * value) { return 0; }
+    int set_view(const std::string_view key, const char * value); // expected-note 2 {{overload defined here}}
 
     void single_overload(const std::string_view key, const char * value) { }
 };
 
-inline int VIID::set_view(StringRef key, const char * value) { return 0; }
-
-struct DateTime {};
-
-struct DateTimeFactory
-{
-    DateTime create_from_string(const char * local_date) const { return {}; }
-    DateTime create_from_string(const StringRef local_date) const // expected-note {{overload defined here}}
-    { return create_from_string(local_date.to_string().c_str()); }
-};
-
-}
 
 std::string get_string() { return "qweqwe"; }
 
@@ -46,18 +27,15 @@ struct RecursionExample
 int main()
 {
     std::string s;
-    tbricks::types::VIID viid;
-    viid.set_ref(s.c_str(), "qweqwe"); // expected-warning {{call to c_str can be removed, overload with std::string_view available}}
-    viid.set_ref("qwe", "qweqwe");
-    viid.set_ref(get_string().c_str(), "qweqwe"); // expected-warning {{call to c_str can be removed, overload with std::string_view available}}
+    Container c;
+    c.set_ref(s.c_str(), "qweqwe"); // expected-warning {{call to c_str can be removed, overload with std::string_view available}}
+    c.set_ref("qwe", "qweqwe");
+    c.set_ref(get_string().c_str(), "qweqwe"); // expected-warning {{call to c_str can be removed, overload with std::string_view available}}
 
-    viid.set_view(s.c_str(), "qweqwe"); // expected-warning {{call to c_str can be removed, overload with types::StringRef available}}
-    viid.set_view("qwe", "qweqwe");
-    viid.set_view(get_string().c_str(), "qweqwe"); // expected-warning {{call to c_str can be removed, overload with types::StringRef available}}
+    c.set_view(s.c_str(), "qweqwe"); // expected-warning {{call to c_str can be removed, overload with std::string_view available}}
+    c.set_view("qwe", "qweqwe");
+    c.set_view(get_string().c_str(), "qweqwe"); // expected-warning {{call to c_str can be removed, overload with std::string_view available}}
 
-    viid.single_overload(s.c_str(), "qwe"); // expected-warning {{call to c_str can be removed, implicit cast from std::string is possible}}
-    viid.single_overload(std::string("test").c_str(), "qwe"); // expected-warning {{call to c_str can be removed, implicit cast from std::string is possible}}
-
-    tbricks::types::DateTimeFactory f;
-    f.create_from_string(s.c_str()); // expected-warning {{call to c_str can be removed, overload with types::StringRef available}}
+    c.single_overload(s.c_str(), "qwe"); // expected-warning {{call to c_str can be removed, implicit cast from std::string is possible}}
+    c.single_overload(std::string("test").c_str(), "qwe"); // expected-warning {{call to c_str can be removed, implicit cast from std::string is possible}}
 }
